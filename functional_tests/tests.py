40471d9dbede50e16c4c3750c68133a9ae8f7a08
ppalacios <pablo@sleepy.com.br>
2015-02-06 02:59:28 -0200

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,25 @@
+import sys
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         """
@@ -25,7 +41,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         # Maria ouviu falar sobre um app de listas bacana.
         # Ela dá uma olhada na homepage do projeto
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # Ela nota que o título da página e o cabeçalho
         # citam to-do lists
@@ -73,7 +89,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # João visita a home page. Não há nenhum sinal da lista da
         # Maria
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Comprar feijão', page_text)
         self.assertNotIn('Cozinhar o feijão', page_text)
@@ -96,7 +112,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Maria vai para a home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # Ela nota que o botão de input está centralizado
