a49025bccf9809de0fbec35826b510ca51f9d4f7
ppalacios <pablo@sleepy.com.br>
2015-02-11 01:23:13 -0200

    Added FT for input validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,16 +6,28 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Maria vai para a página inicial e acidentalmente submete
         # uma lista vazia. Ela aperta enter com a caixa vazia
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # A página atualiza e há uma mensagem de erro dizendo que
         # uma lista não pode ser iniciada com itens em branco
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # Ela tenta novamente com algum texto para um item, o que faz
         # funcionar agora
+        self.browser.find_element_by_id('id_new_item').send_keys('Comprar leite\n')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversa, ela tenta submeter mais um item em branco na lista
+        self.browser.find_element_by_id('id_new_item').send_keys('Comprar leite\n')
 
         # Ela recebe uma mensagem similar na página da lista
-
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+        
         # E ela corrige seu erro colocando algum texto no input
-        self.fail('write me')
+        self.browser.find_element_by_id('id_new_item').send_keys('Fazer chá\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Fazer chá')
